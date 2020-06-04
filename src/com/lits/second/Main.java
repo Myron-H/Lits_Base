99d03f15dae72792630d375a9a0d4563250f6298
Myron-H <hanets1973@gmail.com>
2020-06-04 16:56:27 +0300

    Added check whether a string is a Palindrome

--- src/com/lits/second/Main.java
+++ src/com/lits/second/Main.java
@@ -1,7 +1,23 @@
 package com.lits.second;
 
+import java.util.Scanner;
+
+import static com.lits.second.Test.Polindrom;
+
 public class Main {
     public static void main(String[] args) {
+                     // Поліндром
+        System.out.println("Завдання 1: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Веедіть слово для перевірки:" + str);
+
+        if (Polindrom(str))    // Приймаємо результат з методу Polindrome і Виводимо в консоль
+            System.out.println("Так, це поліндром");
+        else
+            System.out.println("Ні, це не поліндром");
+
+                    //
         Test tst = new Test();
         tst.goo();
         System.out.println(" ");
@@ -12,6 +28,8 @@ public class Main {
         zoo.max.say();
         zoo.cat.sayAnimal("Мурка", 8, "персицька");
         zoo.dog.sayAnimal("Рекс", 12, "доберман");
+
+
     }
 
 }
--- src/com/lits/second/Test.java
+++ src/com/lits/second/Test.java
@@ -30,4 +30,19 @@ public class Test {
         }
         // Результат: 1 2 3 3 2 1
     }
+
+    public static Boolean Polindrom(String str) { // приймаємо слово яке хочимо перевірити
+
+        int i = 0, j = str.length() - 1;  // змінна i на початкову букву, j на кінцеву
+
+        while (i < j) {          // працює поки i<j
+
+            if (str.charAt(i) != str.charAt(j)) // змінюємо букву з стрінга на символ в char
+                return false;                   // якщо символи не однакові то вертаємо false
+            i++;
+            j--;
+
+        }
+        return true;                       //  якщо є співпадіння вертаємо true
+    }
 }
