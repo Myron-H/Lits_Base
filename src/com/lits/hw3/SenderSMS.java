f2b158a635bc41f3b28947bae522e87669853f77
Myron-H <hanets1973@gmail.com>
2020-06-15 18:14:26 +0300

    Deleted

--- src/com/lits/hw3/SenderSMS.java
+++
@@ -1,9 +0,0 @@
-package com.lits.hw3;
-
-public class SenderSMS extends People {
-    // Перезаписуєм метод, поліморфизм
-    @Override
-    public void sendToCenter() {
-        System.out.println("SOS SOS SOS, палива тільки вистачить на" + SpaceX.distanceMks + " км");
-    }
-}
